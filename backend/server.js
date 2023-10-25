8a24c6bd0f7c2170f9d71b961a8fc7407814be25
Benjamin-expertDev <benjaminmwambura@gmail.com>
2023-10-25 19:53:15 +0300

    initial commit

---
+++ backend/server.js
@@ -0,0 +1,28 @@
+const express = require("express")
+const connectDB = require("./config/connectDB")
+const dotenv = require ("dotenv").config();
+const mongoose = require("mongoose");
+const Task = require("./models/taskModels");
+const taskRoutes = require ("./routes/taskRoute")
+
+const app = express()
+//middleware
+app.use(express.json()),
+app.use(express.urlencoded({extended:false}));
+app.use(taskRoutes); 
+
+//Routes
+app.get ("/", (req, res)=> {
+    res.send("Home page")
+})
+
+const PORT = process.env.PORT || 5000 
+
+mongoose
+.connect(process.env.MONGO_URI)
+.then(()=>{
+    app.listen(PORT, ()=>{
+        console.log(`Server running on port ${PORT}`);
+    });
+})
+.catch((err)=>console.log(err))
