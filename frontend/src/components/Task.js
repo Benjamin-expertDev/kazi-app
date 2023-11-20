949bd24d4d90cc7fb09c1ba1fb4e3b541257b230
Benjamin-expertDev <benjaminmwambura@gmail.com>
2023-11-20 12:51:21 +0300

    Task listing

--- frontend/src/components/Task.js
+++ frontend/src/components/Task.js
@@ -1,6 +1,17 @@
+import {FaCheckDouble, FaEdit, FaRegTrashAlt}  from "react-icons/fa"
 const Task = () => {
   return (
-    <div>Task</div>
+    <div className="task">
+      <p>
+        <b>1.</b>
+        Task 1
+      </p>
+      <div className="task-icons">
+      <FaCheckDouble color = "green"/>
+      <FaEdit color = "purple"/>
+      <FaRegTrashAlt color = "red"/>
+      </div>
+    </div>
   )
 }
 
--- frontend/src/components/TaskList.js
+++ frontend/src/components/TaskList.js
@@ -1,10 +1,24 @@
 import TaskForm from "./TaskForm"
-
+import Task from "./Task"
+import { useState } from "react"
 const TaskList = () => {
+ const [formData, setFormData] = useState({
+
+ })
   return ( 
     <div>
        <h2>Task Manager</h2>
        <TaskForm />
+       <div className="--flex-between --pb">
+       <p>
+            <b>Total Task: </b>0
+        </p>
+        <p>
+          <b>Completed Tasks: </b>0
+        </p>
+       </div>
+       <hr/>
+       <Task />
     </div>
     
   )
